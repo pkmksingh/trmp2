a304c1c1b42ded8fd7456a063e88d4c313aa1eca
pkmksingh <picturesmk111@gmail.com>
2025-09-06 02:29:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+TWITCH_URL = "https://www.twitch.tv/randomtodaytv"
+
+RTMP_DESTINATIONS = [
+    "rtmp://your.server.com/live/stream1",
+    "rtmp://another.server.com/live/stream2"
+]
+
+FALLBACK_VIDEO = "fallback.mp4"
